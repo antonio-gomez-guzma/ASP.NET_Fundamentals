fc5c9d9d68c04dbe11c47bc8c22ef768c7498005
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-16 10:26:49 +0200

    Formatters and content negotiation

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -5,13 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-});
+}).AddXmlDataContractSerializerFormatters();
 
 builder.Services.AddProblemDetails(options =>
 {
     options.CustomizeProblemDetails = ctx =>
     {
         ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
+        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
     };
 });
 
